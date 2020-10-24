db010d0113deace3a7b13433298c77083b111804
Crowdbotics <team@crowdbotics.com>
2020-10-24 15:53:44 +0000

    Update components

--- src/features/BlankScreen0146564/screens/blank.js
+++ src/features/BlankScreen0146564/screens/blank.js
@@ -23,11 +23,27 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { TextInput_9: "" }
 
   render = () => (
     <View style={styles.View_1}>
-      <View style={styles.View_3} />
+      <View style={styles.View_3}>
+        <Icon name="bars" />
+        <TextInput
+          placeholder="Sample text input placeholder"
+          style={styles.TextInput_9}
+          value={this.state.TextInput_9}
+          onChangeText={nextValue => this.setState({ TextInput_9: nextValue })}
+        />
+        <Image
+          resizeMode="contain"
+          source={{
+            uri:
+              "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/commentQ_Logo.png"
+          }}
+          style={styles.Image_26}
+        />
+      </View>
     </View>
   )
 }
@@ -37,17 +53,43 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {
     width: "100%",
     height: "100%",
     alignSelf: "flex-start",
     alignContent: "flex-start"
   },
+
   View_3: {
     width: "100%",
     height: 20,
     alignSelf: "flex-start",
     alignContent: "flex-start",
     borderWidth: 1
-  }
+  },
+  View_1: {
+    width: "100%",
+    height: "100%",
+    flexDirection: "row",
+    alignContent: "flex-start"
+  },
+  View_3: {
+    width: "100%",
+    height: 45,
+    marginTop: 25,
+    overflow: "hidden",
+    alignSelf: "flex-start",
+    justifyContent: "center",
+    alignContent: "center",
+    borderBottomWidth: 1
+  },
+  Icon_6: {},
+  TextInput_9: {
+    width: "70%",
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: "center"
+  },
+  Image_26: { width: 30, height: 30, alignSelf: "flex-start" }
 })
